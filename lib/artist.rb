2012b883db39c1e0580456bc2cecce0f2de8f5ab
Alberto <ajsaenz1091@gmail.com>
2021-07-03 14:44:52 -0400

    commit tree 1da8d507392bc9e8a960834656843d41cd1d8c9f

---
+++ lib/artist.rb
@@ -0,0 +1,37 @@
+class Artist
+    attr_accessor :name
+
+    @@all = []
+
+    def initialize(name)
+        @name = name
+        @songs = []
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
+
+    def songs
+        Song.all.filter{|song| song.artist == self}
+    end
+
+    def add_song(song)
+        song.artist = self
+    end
+
+    def self.find_or_create_by_name(name)
+        artist = self.all.find{|artist| artist.name == name}
+        if artist
+            artist
+        else
+            Artist.new(name)
+        end
+    end
+
+    def print_songs
+        songs.each{|song| puts song.name}
+    end
+
+end
